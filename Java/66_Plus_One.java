94af3926e06edf2cc9ee5d107cabce40a1a2ff26
MrP29 <59458329+MrP29@users.noreply.github.com>
2021-04-28 12:46:32 -0400

    Update 66_Plus_One.java

--- Java/66_Plus_One.java
+++ Java/66_Plus_One.java
@@ -1,5 +1,22 @@
 class Solution {
 	public int[] plusOne(int[] digits) {
+		int n = digits.length;
+		for(int i = n - 1; i >= 0; i--) {
+			if(digits[i] < 9) {
+				digits[i]++;
+				return digits;
+			}
+			digits[i] = 0;
+		}
+		digits = new int[n + 1];
+		digits[0] = 1;
+
+		return digits;
+	}
+}
+
+class Solution {
+	public int[] plusOne(int[] digits) {
 		boolean up = true;
 		for(int i = 0; up && i < digits.length; i++) {
 			if(digits[digits.length - 1 - i] == 9) {
@@ -10,7 +27,7 @@ class Solution {
 			}
 		}
 
-		if(up && digits.length != 100) {
+		if(up) {
 			digits = new int[digits + 1];
 			digits[0] = 1;
 		}
