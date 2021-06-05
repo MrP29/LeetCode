b921b9f472d02f7cf1011c7fd0c31a9a9dbf3d7a
MrP29 <pokkm29@gmail.com>
2021-06-05 11:31:15 -0400

    88_Update_Java

--- Array/Java/88_Merge_Sorted_Array.java
+++ Array/Java/88_Merge_Sorted_Array.java
@@ -1,14 +1,12 @@
 //Three Pointer (Time: O(n + m) Space: O(1))
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = m + n - 1;
-        m--;
-        n--;
-        while(i >= 0 && n >= 0) {
-            if(m >= 0 && nums1[m] > nums2[n]) {
-                nums1[i--] = nums1[m--];
+        int i = nums1.length;
+        while(i >= 0 && n > 0) {
+            if(m > 0 && nums1[m - 1] > nums2[n - 1]) {
+                nums1[--i] = nums1[--m];
             } else {
-                nums1[i--] = nums2[n--];
+                nums1[--i] = nums2[--n];
             }
         }
     }
