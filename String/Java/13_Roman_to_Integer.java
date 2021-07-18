1be5fb20881ed1a11fa69f9f72b2c259e2b9fe89
MrP29 <pokkm29@gmail.com>
2021-07-18 14:46:45 -0400

    13_Update_Java

--- String/Java/13_Roman_to_Integer.java
+++ String/Java/13_Roman_to_Integer.java
@@ -50,8 +50,6 @@ class Solution {
 
     public int romanToInt(String s) {
         int res = 0;
-        //int val = romans.get(s.charAt(0));
-        //int next = 0;
 
         for(int i = 0; i < s.length(); i++) {
             int val = romans.get(s.charAt(i));
