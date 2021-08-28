450465321ed800216b28a88ec3c006402097644a
MrP29 <pokkm29@gmail.com>
2021-08-27 20:53:35 -0400

    350_Update_Java

--- Array/Java/350_Intersection_of_Two_Arrays_II.java
+++ Array/Java/350_Intersection_of_Two_Arrays_II.java
@@ -3,25 +3,20 @@ class Solution {
 	public int[] intersect(int[] nums1, int[] nums2) {
 		HashMap<Integer, Integer> map = new HashMap<>();
 		List<Integer> arr = new ArrayList<>();
-		for(int num: nums1) {
-			if(map.containsKey(num))
-				map.put(num, map.get(num) + 1);
-			else
-				map.put(num, 1);
-		}
+		for(int num: nums1)
+            map.put(num, map.getOrDefault(num, 0) + 1);
 
 		for(int num: nums2) {
 			if(map.containsKey(num) && map.get(num) > 0) {
 				arr.add(num);
 				map.put(num, map.get(num) - 1);
 			}
-
 		}
 
 		int[] intersect = new int[arr.size()];
-		for(int i = 0; i < arr.size(); i++) {
+		for(int i = 0; i < arr.size(); i++)
 			intersect[i] = arr.get(i);
-		}
+
 		return intersect;
 	}
 }
