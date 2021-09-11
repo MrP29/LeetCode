ca98c17d9fdd0b23a52f9b5268cbed752b1d8967
MrP29 <pokkm29@gmail.com>
2021-09-11 16:16:02 -0400

    70_Update_Java

--- Dynamic_Programming/Java/70_Climbing_Stairs.java
+++ Dynamic_Programming/Java/70_Climbing_Stairs.java
@@ -1,4 +1,4 @@
-//Iterative Fibonacci Sequence (Time: O(n) Space: O(1))
+//Iterative Fibonacci Sequence (Time: O(N) Space: O(1))
 class Solution {
     public int climbStairs(int n) {
         if(n == 1)
@@ -17,7 +17,29 @@ class Solution {
     }
 }
 
-//Recursive Fibonacci Sequence(Time: O(n) Space: O(n))
+//Recursive Fibonacci Sequence(Time: O(N) Space: O(N))
+public class Solution {
+    int[] steps;
+    
+    public int climbStairs(int n) {
+        steps = new int[n + 1];
+        return stairsHelp(n);
+    }
+    
+    private int stairsHelp(int n) {
+        if(n < 0)
+            return 0;
+        if(n == 0)
+            return 1;
+        if(steps[n] > 0)
+            return steps[n];
+        
+        steps[n] = stairsHelp(n - 1) + stairsHelp(n - 2);
+        return steps[n];
+    }
+}
+
+//Recursive Fibonacci Sequence(Time: O(N) Space: O(N))
 public class Solution {
     int[] steps;
     
@@ -40,7 +62,7 @@ public class Solution {
     }
 }
 
-//Fibonacci Formula by LeetCode (Time: O(logn) Space: O(1))
+//Fibonacci Formula by LeetCode (Time: O(logN) Space: O(1))
 //Fn = 1 / sqrt5 [((1 + sqrt5) / 2)^n - ((1 - sqrt5) / 2)^n]
 public class Solution {
     public int climbStairs(int n) {
