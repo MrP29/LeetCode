0eb7a1439fdbbe857ddde8b2260b335ee572a170
MrP29 <pokkm29@gmail.com>
2021-09-23 23:17:23 -0400

    101_Update_Java

--- Tree/Java/101_Symmetric_Tree.java
+++ Tree/Java/101_Symmetric_Tree.java
@@ -22,11 +22,11 @@ class Solution {
     private boolean isSymmetric(TreeNode left, TreeNode right) {
         if(left == null && right == null)
             return true;
-        if((left != null && right == null) || (left == null && right != null))
+
+        if(left == null || right == null)
             return false;
-        if(left.val != right.val)
-            return false;
-        return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+
+        return (left.val == right.val) && isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
     }
 }
 
@@ -71,7 +71,7 @@ class Solution {
             if(left == null && right == null)
                 continue;
             
-            if((left == null && right != null) || (left != null && right == null))
+            if((left == null || right == null))
                 return false;
             
             if(left.val != right.val)
