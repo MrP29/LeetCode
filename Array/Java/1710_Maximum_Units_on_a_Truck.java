ad6f3e7ca312524cb3a5cb44516b2186d6ce2843
MrP29 <pokkm29@gmail.com>
2021-11-19 17:21:09 -0500

    Update_1710_Java

--- Array/Java/1710_Maximum_Units_on_a_Truck.java
+++ Array/Java/1710_Maximum_Units_on_a_Truck.java
@@ -1,14 +1,14 @@
 //Array Sorting (Time: O(NlogN) Space: O(1))
 class Solution {
     public int maximumUnits(int[][] boxTypes, int truckSize) {
-        Arrays.sort(boxTypes, (a, b) -> b[1] - a[1]);
+        Arrays.sort(boxTypes, (a, b) -> Integer.compare(b[1], a[1]));
         
         int ans = 0;
         for(int i = 0; i < boxTypes.length && truckSize > 0; i++) {
             ans += boxTypes[i][1] * Math.min(boxTypes[i][0], truckSize);
             truckSize -= boxTypes[i][0];
         }
-        
+
         return ans;
     }
 }
@@ -16,17 +16,16 @@ class Solution {
 //Priority Queue by LeetCode (Time: O(NlogN) Space: O(N))
 class Solution {
     public int maximumUnits(int[][] boxTypes, int truckSize) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[1] - a[1]);
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(b[1], a[1]));
         pq.addAll(Arrays.asList(boxTypes));
         
         int ans = 0;
-        while(!pq.isEmpty()) {
+        while(truckSize > 0 && !pq.isEmpty()) {
             int[] box = pq.poll();
             ans += box[1] * Math.min(box[0], truckSize);
             truckSize -= box[0];
-            if(truckSize <= 0)
-                break;
         }
+
         return ans;
     }
 }
