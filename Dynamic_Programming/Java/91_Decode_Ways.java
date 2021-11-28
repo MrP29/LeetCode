e5c350226acac6cc02a33374a2c9eb0c9a3a39d0
MrP29 <pokkm29@gmail.com>
2021-11-27 23:07:41 -0500

    Update_91_Java

--- Dynamic_Programming/Java/91_Decode_Ways.java
+++ Dynamic_Programming/Java/91_Decode_Ways.java
@@ -1,145 +1,80 @@
-//Dynamic Programming
+//Dynamic Programming (Time: O(N) Space: O(1))
 class Solution {
     public int numDecodings(String s) {
+        if(s.charAt(0) == '0')
+            return 0;
+        
         int n = s.length();
-        int[] dp = new int[n];
+        int twoBefore = 1;
+        int oneBefore = 1;
         
-        for(int i = 0; i < n; i++) {
-            if(i == 0) {
-                if(mapped(s, i, i + 1))
-                    dp[0] = 1;
-            } else if(i == 1) {
-                if(mapped(s, i, i + 1))
-                    dp[1] = dp[0];
-                if(mapped(s, i - 1, i + 1))
-                    dp[1] = dp[1] + 1;
-            } else {
-                if(dp[i - 1] != 0 && mapped(s, i, i + 1))
-                    dp[i] = dp[i - 1];
-                if(dp[i - 2] != 0 && mapped(s, i - 1, i + 1))
-                    dp[i] = dp[i - 2] + dp[i];
-            }
+        for(int i = 1; i < n; i++) {
+            int cur = 0;
+            if(s.charAt(i) != '0')
+                cur = oneBefore;
+            
+            int doubleDigit = Integer.valueOf(s.substring(i - 1, i + 1));
+            if(doubleDigit > 9 && doubleDigit < 27)
+                cur += twoBefore;
+            
+            twoBefore = oneBefore;
+            oneBefore = cur;
         }
-        
-        return dp[n - 1];
-    }
-    
-    private boolean mapped(String s, int start, int end) {
-        if(start + 1 == end) {
-            int n1 = Integer.valueOf(s.substring(start, end));
-            return n1 != 0;
-        } else {
-            int n1 = Integer.valueOf(s.substring(start, end - 1));
-            int n2 = Integer.valueOf(s.substring(start, end));
-            return n1 != 0 && n2 < 27;
-        }
+        return oneBefore;
     }
 }
 
-//Time Out
+//Dynamic Programming with Array (Time: O(N) Space: O(N))
 class Solution {
     public int numDecodings(String s) {
-        int n = s.length();
-        int[] dp = new int[n];
-
-        if(s.length() > 0) {
-            if(mapped(s, 0, 1))
-                dp[0] = 1;
-        }
+        if(s.charAt(0) == '0')
+            return 0;
         
-        if(s.length() > 1) {
-            if(mapped(s, 1, 2))
-                dp[1] = dp[0];
-            if(mapped(s, 0, 2))
-                dp[1] = dp[0] + 1;
-        }
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
                         
-        for(int i = 2; i < n; i++) {
-            if(dp[i - 1] != 0 && mapped(s, i, i + 1))
-                dp[i] = dp[i - 1];
-            if(dp[i - 2] != 0 && mapped(s, i - 1, i + 1))
-                dp[i] = dp[i - 2] + dp[i];
+        for(int i = 1; i < n; i++) {
+            if(s.charAt(i) != '0')
+                dp[i + 1] = dp[i];
+            
+            int doubleDigit = Integer.valueOf(s.substring(i - 1, i + 1));
+            if(doubleDigit > 9 && doubleDigit < 27)
+                dp[i + 1] += dp[i - 1];
         }
         
-        return dp[n - 1];
-    }
-    
-    private boolean mapped(String s, int start, int end) {
-        if(start + 1 == end) {
-            int n1 = Integer.valueOf(s.substring(start, end));
-            return n1 != 0;
-        } else {
-            int n1 = Integer.valueOf(s.substring(start, end - 1));
-            int n2 = Integer.valueOf(s.substring(start, end));
-            return n1 != 0 && n2 < 27;
-        }
+        return dp[n];
     }
 }
 
-
-
+//Recursive Dynamic Programming with Memoization by LeetCode (Time: O(N) Space: O(N))
 class Solution {
-    int decoded;
+    Map<Integer, Integer> memo = new HashMap();
     
     public int numDecodings(String s) {
-        decoded = 0;
-        decode(s);
-        return decoded;
-    }
-        
-    private void decode(String s) {
-        if(s.length() == 0) {
-            decoded++;
-            return;
-        }
-
-        int n1 = Integer.valueOf(s.substring(0, 1));
-        if(n1 == 0) //< 1 || n1 > 26)
-            return;
-        decode(s.substring(1, s.length()));
-
-
-        if(s.length() < 2)
-            return;
-        
-        if(Integer.valueOf(s.substring(0, 2)) <= 26)
-            decode(s.substring(2, s.length()));
-    }
-}
-*/
-
-
-//Time Out
-class Solution {
-    Set<String> codes;
-    int decoded;
-    public int numDecodings(String s) {
-        init();
-        decoded = 0;
-        
-        decode(s);
-        return decoded;
+        return decode(0, s);
     }
     
-    private void decode(String s) {
-        if(s.length() == 0) {
-            decoded++;
-            return;
-        }
-    
-        if(s.length() > 0 && codes.contains(s.substring(0, 1))) {
-            decode(s.substring(1, s.length()));
-        }
+    private int decode(int index, String str) {
+        if(memo.containsKey(index))
+            return memo.get(index);
         
-        if(s.length() > 1 && codes.contains(s.substring(0, 2))) {
-            decode(s.substring(2, s.length()));
-        }
-    }
-    
-    private void init() {
-        codes = new HashSet();
-        for(int i = 1; i <= 26; i++) {
-            codes.add(String.valueOf(i));
-        }
+        if(index == str.length())
+            return 1;
+        
+        if(str.charAt(index) == '0')
+            return 0;
+        
+        if(index == str.length() - 1)
+            return 1;
+        
+        int ans = decode(index + 1, str);
+        if(Integer.valueOf(str.substring(index, index + 2)) < 27)
+            ans += decode(index + 2, str);
+        
+        memo.put(index, ans);
+        
+        return ans;
     }
 }
