514d29b26aea1a4a415e92fcfc31790d54f4dd25
MrP29 <pokkm29@gmail.com>
2022-04-04 12:58:26 -0400

    Update_1_C++

--- Array/C++/1_Two_Sum.cpp
+++ Array/C++/1_Two_Sum.cpp
@@ -8,13 +8,15 @@ public:
         for(int i = 0; i < nums.size(); i++) {
             int compliment = target - nums[i];
             if(numDict.find(compliment) != numDict.end()) {
-                ans.push_back(i);
-                ans.push_back(numDict.at(compliment));
-                return ans;
+                // ans.push_back(i);
+                // ans.push_back(numDict.at(compliment));
+                // return ans;
+                return {numDict[compliment], i};
             }
             numDict[nums[i]] = i;
         }
-        return ans;
+        // return ans;
+        return {};
     }
 };
 
@@ -22,17 +24,19 @@ public:
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> ans;
+        // vector<int> ans;
         
         for(int i = 0; i < nums.size(); i++) {
             for(int j = i + 1; j < nums.size(); j++) {
                 if(nums[i] + nums[j] == target) {
-                    ans.push_back(i);
-                    ans.push_back(j);
-                    return ans;
+                    // ans.push_back(i);
+                    // ans.push_back(j);
+                    // return ans;
+                    return {i, j};
                 }
             }
         }
-        return ans;
+        // return ans;
+        return {};
     }
 };
